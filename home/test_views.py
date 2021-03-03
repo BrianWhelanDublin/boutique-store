76a7473c4ebb5f154492fe94363feb9fa4dae39a
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-03-03 09:55:37 +0000

    add changes

--- home/test_views.py
+++ home/test_views.py
@@ -5,3 +5,8 @@ class TestDjango(TestCase):
 
     def test_this_works(self):
         self.assertEqual(1, 1)
+    
+    def test_home_view(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/index.html")
